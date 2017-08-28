8a4f832164d3bd5c0984c7091d5a13e35171e409
veer21 <30756147+veer21@users.noreply.github.com>
2017-08-28 16:45:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,24 +106,23 @@ var htmlTemplate=`
  return htmlTemplate;
     
 }
+
+
 app.get('/:articleName', function (req, res) {
     var articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
-app.get('/articleName', function (req, res) {
-  res.send(path.join(createTemplate(articles[articleName])));
- 
-});
-app.get('/article-three', function (req, res) {
-  res.send('article 3 requested here and it will be served here');
-});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
+
+
 app.get('/',function(req,res){
  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
