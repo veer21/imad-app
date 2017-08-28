8383ad4fc399c97a6be8c801dbe9d2953dfbd915
veer21 <30756147+veer21@users.noreply.github.com>
2017-08-28 16:34:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -121,6 +121,9 @@ app.get('/article-three', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
+app.get('/',function(req,res){
+ res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
