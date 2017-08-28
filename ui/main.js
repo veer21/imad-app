6e937665ba612250964ac58bbe4da827827d39d3
veer21 <30756147+veer21@users.noreply.github.com>
2017-08-28 23:41:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,7 @@
 console.log('Loaded!');
  var element=document.getElementById('main-text');
+ 
+ var img=document.getElementById('img');
+ img.onclick=function() {
+     img.style.marginLeft='100px';
+ };
