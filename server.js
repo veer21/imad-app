6339a21baeccee6e84e336754377f85f5e6c3d5a
veer21 <30756147+veer21@users.noreply.github.com>
2017-09-09 17:29:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -128,10 +128,12 @@ app.get('/',function(req,res){
 });
 
 var counter=0;
-app.get('/counter', function (req, res) {
+app.get('/counter', function (req, res) 
+ {
     counter=counter+1;
-  res.send(counter.toString());
-});
+  res.send(counter,toString());
+  }
+);
 
 
 
